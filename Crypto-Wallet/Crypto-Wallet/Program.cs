9ba8c0966a5cc6cf186e2aa5bc9342c82445f0d3
Lovre Tomic <tomiclovre05@gmail.com>
2022-12-03 16:42:03 +0100

    Added createWallet

--- Crypto-Wallet/Crypto-Wallet/Program.cs
+++ Crypto-Wallet/Crypto-Wallet/Program.cs
@@ -1,4 +1,9 @@
-﻿int getInt()
+﻿using Crypto_Wallet.Classes;
+using Crypto_Wallet.Classes.Wallets;
+
+var wallets = new List<Wallet>();
+
+int getInt()
 {
     while(true)
     {
@@ -29,11 +34,12 @@ void MainMenu()
     switch (getInt())
     {
         case 1:
+            createWallet();
             break;
         case 2:
             break;
-        case 3:
-            break;
+        case 0:
+            return;
         default:
             Console.WriteLine("## Pogresan unos! Unos mora biti neka od navedenih opcija.");
             returnToMainMenu(true);
@@ -42,3 +48,35 @@ void MainMenu()
 }
 
 MainMenu();
+
+void createWallet()
+{
+    Console.Clear();
+    Console.WriteLine("--- KREIRANJE WALLETA ---");
+    Console.WriteLine("1 - Bitcoin wallet");
+    Console.WriteLine("2 - Ethereum wallet");
+    Console.WriteLine("3 - Solana wallet");
+    Console.WriteLine("0 - Povratak na glavni izbornik");
+    switch (getInt())
+    {
+        case 1:
+            wallets.Add(new BitcoinWallet());
+            Console.WriteLine("Uspjesno kreiran Bitcoin wallet!");
+            returnToMainMenu(true);
+            break;
+        case 2:
+            wallets.Add(new EthereumWallet());
+            Console.WriteLine("Uspjesno kreiran Ethereum wallet!");
+            returnToMainMenu(true);
+            break;
+        case 3:
+            wallets.Add(new SolanaWallet());
+            Console.WriteLine("Uspjesno kreiran Solana wallet!");
+            returnToMainMenu(true);
+            break;
+        default:
+            Console.WriteLine("## Pogresan unos! Unos mora biti neka od navedenih opcija.");
+            returnToMainMenu(true);
+            break;
+    }
+}
